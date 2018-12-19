92dd2b1321d48e1247fce52da60f73eceeb1cbae
Nic Doorlay <n.doorlay@gmail.com>
2018-12-19 15:44:18 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
-/* Not sure what this program will do yet.
- * Still thinking about it.
- * I'll update this as I go along.
+/* I'm using this program as a way
+ * to test out if else statements.
+ * Too bad my computer doesn't have any space
+ * for an IDE, so I can't run it. I'll do that
+ * later I guess.
  */
  
+#include <iostream>
