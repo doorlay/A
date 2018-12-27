658d7036833a484c208861a0e80ceb0e2f539f16
Nic Doorlay <n.doorlay@gmail.com>
2018-12-27 10:19:54 -0600

    Update and rename main.cpp to main.py

--- main.cpp
+++
@@ -1,8 +0,0 @@
-/* I'm using this program as a way
- * to test out if else statements.
- * Too bad my computer doesn't have any space
- * for an IDE, so I can't run it. I'll do that
- * later I guess.
- */
- 
-#include <iostream>
