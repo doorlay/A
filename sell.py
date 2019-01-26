f908fb86e85beb3e5f2ece3d17399d978cdc8bcf
Nic Doorlay <n.doorlay@gmail.com>
2019-01-26 09:10:51 -0800

    Update sell.py

--- sell.py
+++ sell.py
@@ -1,4 +1,8 @@
-# This program will be used to manage stocks.
+# This file will manage the selling of stocks.
+
+# When a stock drops 8% from purchase price, always sell.
 
 
-# All stocks are sold
+# When a stock rises 20% from purchase price, must then evaluate how quickly this happened.
+# - if the rise took longer than 4 weeks, sell.
+# - otherwise, hold on for 8 total weeks, and then reevaluate.
