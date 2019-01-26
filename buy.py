d1958bb9697b65dca43062cd634d41e397a29318
Nic Doorlay <n.doorlay@gmail.com>
2019-01-26 09:19:40 -0800

    Update buy.py

--- buy.py
+++ buy.py
@@ -1,1 +1,12 @@
 # This file will manage the purchasing of stocks.
+
+
+# This is the most critical part of the process, and if purchases are made at the right time, everything else should go smoothly.
+
+# If a stock is on the way down, do not purchase. Only purchase on the way up.
+
+# Must identify proper buy points. This can include...
+# - cup with handle
+# - double bottom
+# - flat base 
+
