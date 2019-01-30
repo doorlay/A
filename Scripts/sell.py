392f77d8ce80c4ca9a30a5cbf6e5170d3c4d8652
Nic Doorlay <n.doorlay@gmail.com>
2019-01-30 11:32:27 -0800

    Update sell.py

--- Scripts/sell.py
+++ Scripts/sell.py
@@ -1,8 +1,4 @@
-# This file will manage the selling of stocks.
-
-# When a stock drops 8% from purchase price, always sell.
 
 
-# When a stock rises 20% from purchase price, must then evaluate how quickly this happened.
-# - if the rise took longer than 4 weeks, sell.
-# - otherwise, hold on for 8 total weeks, and then reevaluate.
+
+currentprice 
