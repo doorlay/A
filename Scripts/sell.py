35982b0c3f9c5050dafcbbf029c2ccd160b3f86d
Nic Doorlay <n.doorlay@gmail.com>
2019-01-30 12:22:12 -0800

    Update sell.py

--- Scripts/sell.py
+++ Scripts/sell.py
@@ -5,3 +5,9 @@
 # purchase price
 # date of purchase price
 # current date
+
+def timecheck():
+  # Function that checks if it's been four weeks or not.
+
+if (current price / purchase price >= .20):
+  timecheck()
