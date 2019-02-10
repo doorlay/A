89688a1f7f1e0f76807d2d4634f80b0a43f82ac7
Nic Doorlay <n.doorlay@gmail.com>
2019-02-09 17:26:05 -0800

    Update scrape.py

--- Scripts/scrape.py
+++ Scripts/scrape.py
@@ -1,3 +1,9 @@
 # This file will handle gathering the stock data from the internet.
 
+from alpha_vantage.timeseries import TimeSeries
+from pprint import pprint
+ts = TimeSeries(key='52UIN9CWN6RNDHXJ', output_format='pandas')
+data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+pprint(data.head(2))
+
 # The info will then be sent through the other files to be analyzed.
