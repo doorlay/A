f2b2a1fddb6f40182d607dc9b703211ea03f3bd2
Nic Doorlay <n.doorlay@gmail.com>
2019-02-20 09:06:44 -0800

    Update DBscrape.py

--- Database/DBscrape.py
+++ Database/DBscrape.py
@@ -1,3 +1,5 @@
 # This file will scrape the data from the database to be turned into an array.
 
 database1 = open(Database/database.txt)
+database2 = database1.read()
+DBarray = database2.splitlines()
