118d9ccf3c371d06c42c0848bb87f0c8f515d83a
Nic Doorlay <n.doorlay@gmail.com>
2019-03-01 08:23:16 -0800

    Update scrape.py

--- Scripts/scrape.py
+++ Scripts/scrape.py
@@ -18,3 +18,14 @@ data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='ful
 pprint(data.head(2))
 
 # The info will then be sent through the other files to be analyzed.
+
+# below is a psuedo-code example of how I want my data fetching function to run.
+# This function will be imported to both the sell.py file and the buy.py file, in order to fetch real time prices on command.
+
+# parameter accepted will be the stock symbol 
+function getDataFrom(nameofstock):
+	
+	# will create a list containing the below info
+	data = [stockSymbol, purchasePrice, purchaseDate]
+	# returns the data
+	return data
