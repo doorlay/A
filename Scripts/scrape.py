33c7c5ddf397fbad35f0f383e15766e79744de81
Nic Doorlay <n.doorlay@gmail.com>
2019-03-03 09:00:30 -0800

    Update scrape.py

--- Scripts/scrape.py
+++ Scripts/scrape.py
@@ -17,16 +17,15 @@ def scr():
 # Assigns ts to an api call that uses the user's api key
 ts = TimeSeries(key='52UIN9CWN6RNDHXJ')
 
-# Assings data to the json output from the api call
+# Assings data to the json output from the api call. I believe this is actually a tuple, in python?
+# That would explain why I keep getting errors when I use json.loads, becuase it's already in a python format in a tuple.
 data = ts.get_intraday(symbol='MSFT', outputsize='compact')
-print(type(data))
 
 # Creates jsonParsed, a dictionary, from the parsed json data
 # below is commented out for now, as its giving me an error. Need to figure out a way to get the data into a python dictionary.
-jsonParsed = json.loads(data)
-print(type(jsonParsed))
+# jsonParsed = json.loads(data)
 # prints data to test out the corresponding parts the dictionary needs
-# print(data)
+print(data)
 
 # The info will then be sent through the other files to be analyzed.
 
