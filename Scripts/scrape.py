fd129e0e91a4897a5cc2752cea3b61d58a17f5c6
Nic Doorlay <n.doorlay@gmail.com>
2019-03-03 15:42:22 -0800

    Update scrape.py

--- Scripts/scrape.py
+++ Scripts/scrape.py
@@ -21,11 +21,9 @@ ts = TimeSeries(key='52UIN9CWN6RNDHXJ')
 # That would explain why I keep getting errors when I use json.loads, becuase it's already in a python format in a tuple.
 data = ts.get_intraday(symbol='MSFT', outputsize='compact')
 
-# Creates jsonParsed, a dictionary, from the parsed json data
 # below is commented out for now, as its giving me an error. Need to figure out a way to get the data into a python dictionary.
-# jsonParsed = json.loads(data)
 # prints data to test out the corresponding parts the dictionary needs
-print(type(data))
+print(data)
 
 # The info will then be sent through the other files to be analyzed.
 
