e14e30cca9be4282eb76f1a83ad5e0f15b598576
Nic Doorlay <n.doorlay@gmail.com>
2019-03-06 08:00:16 -0800

    Update buy.py

--- Scripts/buy.py
+++ Scripts/buy.py
@@ -1,6 +1,6 @@
 # This file will manage the purchasing of stocks.
 
-
+from scrape import getDataFrom
 # This is the most critical part of the process, and if purchases are made at the right time, everything else should go smoothly.
 
 # If a stock is on the way down, do not purchase. Only purchase on the way up.
@@ -10,3 +10,5 @@
 # - double bottom
 # - flat base 
 
+stock = input("What stock do you want to get data from? Enter with 'quotes':)
+getDataFrom(stock)
