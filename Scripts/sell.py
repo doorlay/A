fa80730694437dd0fe37ddf803dc97e9716a80ef
Nic Doorlay <n.doorlay@gmail.com>
2019-03-10 17:26:42 -0700

    Update sell.py

--- Scripts/sell.py
+++ Scripts/sell.py
@@ -4,23 +4,15 @@ from scrape import scr
 # getData function returns stock symbol, current price, and and current date in a list.
 from scrape import getData
 
-'''
-data needed to make a sell decision:
-
-Stock symbol (done)
-current price (done)
-purchase price
-date of purchase
-current date (done)
-'''
-
 # just need purchase price and date of purchase, which is stored in my database. Perfect!
 
+getData('AMD')
 
-
+'''
 def timecheck():
   
   # Function that checks if it's been four weeks or not.
 
 if (current_price / purchase_price >= .20):
   timecheck()
+'''
