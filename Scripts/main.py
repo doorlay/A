db1b5560649c08f6cd914d4b7977d04f4488113d
Nic Doorlay <nic@doorlay.com>
2019-05-10 10:40:29 -0700

    Delete main.py

--- Scripts/main.py
+++
@@ -1,6 +0,0 @@
-# getData function returns stock symbol, current price, and and current date in a list.
-from scrape import getData
-
-# Currently getting data for Microsoft, but can scrape for any symbol. Just make sure to enter in quotes!
-stockdata = getData('MSFT')
-print(stockdata)
